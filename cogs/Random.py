7f298d50944bfcd044de7db36ce7817efb776042
Sleigh <slayerdeathicus@gmail.com>
2020-11-04 17:05:00 +0900

    Added a few TODOs, minor code cleanup

--- cogs/Random.py
+++ cogs/Random.py
@@ -15,13 +15,21 @@ class Random(commands.Cog):
 
     # Manages the updating (and creation) of the song table whenever needed
     async def manage_song_table(self):
+        # Acquire worksheet instance from cloud
         spreadsheet = await self.gc_client.open("Arcaea Songs")
+        # TODO: When we eventually remove the first manually-made sheet, change the reference to said sheet below
         worksheet = await spreadsheet.worksheet("Song List ArcBot2")
         songs = await worksheet.get_all_values()
 
-    """@commands.command()
+        # Create dictionary of songs
+        title = songs.pop(0)
+        songs = [{k: v for k, v in zip(title, song)} for song in songs]  # Bless dict comprehension tbh
+        # TODO: Push this list of dicts to a new MongoDB collection - look into Motor async
+
+    # TODO: This is obviously only here for debug purposes. Remove when finished with bot.
+    @commands.command()
     async def testing(self, ctx):
-        await ctx.send(await self.manage_song_table())"""
+        await ctx.send(await self.manage_song_table())
 
 
 def setup(bot):
