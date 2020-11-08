cbffe22e1a794fb8565fc2754e90ff6c92455b0d
Sleigh <slayerdeathicus@gmail.com>
2020-11-08 19:04:43 +0900

    - Added bot logging; When bot runs, it will create logs in a logs folder, with ymd-hm format as filename - Added a check to make sure file is running as main()

--- BotCore.py
+++ BotCore.py
@@ -1,27 +1,46 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import logging
+
 import discord
 from discord.ext import commands
 
 import config
 
+from pathlib import Path
+import datetime
 
-class Bot(commands.Bot):
-    def __init__(self, **kwargs):
-        super().__init__(command_prefix=commands.when_mentioned_or('%'), **kwargs)
+def main():
+    """This is the main file of ArcaeaBot. Please run the bot by running this file."""
 
-    async def on_ready(self):
-        for cog in config.cogs:
-            try:
-                self.load_extension(f"cogs.{cog}")
-            except Exception as exc:
-                print(f'Could not load extension {cog} due to {exc.__class__.__name__}: {exc}')
+    class Bot(commands.Bot):
+        def __init__(self, **kwargs):
+            super().__init__(command_prefix=commands.when_mentioned_or('%'), **kwargs)
+            # Logging
+            logger = logging.getLogger('discord')
+            logger.setLevel(logging.INFO)
 
-        await self.change_presence(activity=discord.Game(f"%help | Online on {len(self.guilds)} servers"))
-        print(f'Logged on as {self.user} (ID: {self.user.id})')
+            time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+            handler = logging.FileHandler(filename=Path("logs", f"{time}.log"),
+                                          encoding='utf-8', mode='w')
+            handler.setFormatter(logging.Formatter('%(asctime)s: %(levelname)s: %(name)s: %(message)s'))
+            logger.addHandler(handler)
+
+        async def on_ready(self):
+            for cog in config.cogs:
+                try:
+                    self.load_extension(f"cogs.{cog}")
+                except Exception as exc:
+                    print(f'Could not load extension {cog} due to {exc.__class__.__name__}: {exc}')
+
+            await self.change_presence(activity=discord.Game(f"%help | Online on {len(self.guilds)} servers"))
+            print(f'Logged on as {self.user} (ID: {self.user.id})')
+
+    bot = Bot()
+
+    bot.run(config.TOKEN)
 
 
-bot = Bot()
-
-bot.run(config.TOKEN)
+if __name__ == "__main__":
+    main()
