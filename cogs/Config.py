3ae497e2d81b9196108f802e219f268f269a47c5
Sleigh <slayerdeathicus@gmail.com>
2020-11-08 19:02:38 +0900

    - Removed manage_song_table, because if we already have the data on GCloud, no need to have it on DB again for redundancy - Improved song_list_length and gave it more features and appearance lift-up; See TODOS for future plans

--- cogs/Config.py
+++ cogs/Config.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import itertools
-
 import gspread_asyncio
 import motor.motor_asyncio
+import discord
 from discord.ext import commands
 from google.oauth2 import service_account
 
@@ -11,7 +10,7 @@ import config
 
 
 class Config(commands.Cog):
-    """The description for Config goes here."""
+    """A debug cog used to configure, manage and setup parts of ArcaeaBot."""
 
     def __init__(self, bot):
         # MongoDB
@@ -41,63 +40,43 @@ class Config(commands.Cog):
                 spreadsheet = await gc_client.open("Arcaea Songs")
                 # TODO: When we eventually remove the first manually-made sheet, change reference to sheet below
                 worksheet = await spreadsheet.worksheet("Song List ArcBot2")
-                return await worksheet.get_all_values()
+
+                # Organize songs
+                songs = await worksheet.get_all_values()
+                title = songs.pop(0)
+                title[0] = "_id"
+
+                return [{k: v for k, v in zip(title, song)} for song in songs]  # Bless dict comprehension tbh
 
         bot.songs = await grab_songs(bot, bot.gc_client)
 
-    # Manages the updating (and creation) of the song table whenever needed
-    async def manage_song_table(self):
-        songs = self.bot.songs
-
-        # Create dictionary of songs
-        title = songs.pop(0)
-        title[0] = "_id"
-        songs = [{k: v for k, v in zip(title, song)} for song in songs]  # Bless dict comprehension tbh
-
-        # Push songs to song collection
-        """
-        Workflow process:
-        - Function will compare each song.
-        - If there are differences, the one cached in GSheets will replace the one on DB.
-        - If document does not exist, it will be pushed into DB.
-        - Returns a list of all results inserted.
-        """
-        db_songs = await self.db["Songs"].find().to_list(length=None)
-        result = []
-
-        # If the db has not been constructed at all/corrupted
-        if len(db_songs) == 0:
-            result = len(await self.db["Songs"].insert_many(songs).inserted_ids)
-        else:
-            for document, song in itertools.zip_longest(db_songs, songs, fillvalue={}):
-                if document != song:
-                    i = song.pop("_id")
-                    result.append(await self.db["Songs"].replace_one({"_id": i}, song))
-
-            result = sum([song.modified_count for song in result])
-
-        return result
-
-    # TODO: This is obviously only here for debug purposes. Remove when finished with bot.
-    @commands.command()
-    async def insert_songs(self, ctx):
-        await ctx.send("Please hold, this may take some time...")
-        total = await self.manage_song_table()
-        await ctx.send(f"Modified {total} songs.")
-        await ctx.send("<a:default_dance:583909994504912901>")
-
-    @commands.command()
+    @commands.command(aliases=["dropdb"])
     async def cleardb(self, ctx):
         await self.db.drop_collection("Songs")
         await ctx.send("<:TohruShrug:361977037306724366>")
 
-    # TODO: Maybe I'll keep this around as a debug command. A debug cog, maybe?
-    @commands.command()
+    @commands.command(aliases=["check_db", "checkdb", "songlist", "song_list"])
     async def song_list_length(self, ctx):
-        await ctx.send(f"GSheets currently has {len(self.bot.songs)} songs.")
-        cursor = self.db["Songs"].find()
-        songs = [doc for doc in await cursor.to_list(length=None)]
-        await ctx.send(f"MongoDB currently has {len(songs)} songs.")
+        # TODO: Once we learn webscraping, compare it to the wiki song list instead.
+        # TODO: Call a (sub)command that will update the wiki list to the cloud sheet.
+        embed = discord.Embed(
+            description=f"GSheets currently has {len(self.bot.songs)} songs."
+        ).set_author(name="Checking bot songs sync...")
+
+        msg = await ctx.send(embed=embed)
+
+        embed.description = (f"{embed.description}\n"
+                             f"[Placeholder] currently has [Placeholder] songs.")
+
+        # TODO: Change this condition.
+        if len(self.bot.songs) == len("[Placeholder]"):
+            embed.set_author(name="Bot songs synced!")
+            embed.color = discord.Color.green()
+        else:
+            embed.set_author(name="Bot songs not synced!")
+            embed.color = discord.Color.red()
+
+        await msg.edit(embed=embed)
 
     # TODO: Something like this as well. Perhaps send two, or three dicts per message, so as to not
     #  overload the 2000 char limit, in a nice spool, with the requested amount filling the length parameter.
